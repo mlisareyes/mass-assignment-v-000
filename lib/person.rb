c92bf963ac2e94af8c504d74cd97bf74542ca6c3
Monica Lisa Reyes <mlisareyes@gmail.com>
2018-07-18 06:36:08 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,4 +1,9 @@
 class Person
   #your code here
   attr_accessor :name, :weight, :pant_length
+
+  def initialize(name:, weight:, pant_length:)
+    @name = name
+    @weight = weight
+    @pant_length = pant_length
 end
